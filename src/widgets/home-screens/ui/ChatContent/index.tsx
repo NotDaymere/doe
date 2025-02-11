0b68d1feec8ca293a54e8a4afb0fe695e9050f00
Maksym Meleshko <maks.meleshko12@gmail.com>
2025-02-11 17:43:20 +0200

    added parser and styles for bot answer

--- src/widgets/home-screens/ui/ChatContent/index.tsx
+++ src/widgets/home-screens/ui/ChatContent/index.tsx
@@ -1,12 +1,64 @@
 import React from "react";
-import { useChatController } from "../..";
+
+// TipTap extensions
+import Bold from "@tiptap/extension-bold";
+import Document from "@tiptap/extension-document";
+import History from "@tiptap/extension-history";
+import Italic from "@tiptap/extension-italic";
+import Link from "@tiptap/extension-link";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import Underline from "@tiptap/extension-underline";
+import { useEditor } from "@tiptap/react";
+
+// Shared providers
 import { useChatStore } from "src/shared/providers";
-import css from "./ChatContent.module.less";
+
+// Custom TipTap extensions
+import {
+    CustomCodeBlock,
+    CustomInlineCode,
+    CustomSpan,
+    Div,
+    Formula,
+    createHandleTab,
+} from "src/components/tiptap-editor/extensions/index";
+
+// Chat components
+import { useChatController } from "../..";
 import { ChatMessage } from "../ChatMessage";
+
+// Styles
+import css from "./ChatContent.module.less";
+
 
 export const ChatContent: React.FC = () => {
     const { chatRef } = useChatController();
     const { currentBranch, messages } = useChatStore();
+    const editor = useEditor({
+        extensions: [
+            Div,
+            Document,
+            Text,
+            History.configure({
+                depth: 100,
+                newGroupDelay: 500,
+            }),
+            Paragraph,
+            Bold,
+            Italic,
+            CustomSpan,
+            Formula,
+            Underline,
+            CustomInlineCode,
+            CustomCodeBlock,
+            Link.configure({
+                autolink: false,
+            }),
+            createHandleTab(),
+        ],
+    });
+    
 
     return (
         <div className={css.content}>
@@ -16,6 +68,7 @@ export const ChatContent: React.FC = () => {
                         <ChatMessage 
                             data={item} 
                             key={item.id} 
+                            editor={editor}
                         />
                     ))}
                 </div>
--- src/widgets/home-screens/ui/ChatMessage/index.tsx
+++ src/widgets/home-screens/ui/ChatMessage/index.tsx
@@ -1,14 +1,33 @@
 import React from "react";
+
+// External libraries
+import { Editor as EditorTiptap } from "@tiptap/react";
+import { MathJax } from "better-react-mathjax";
+
+// Shared types & providers
 import { IMessage } from "src/shared/types/Message";
-import PenIcon from "src/shared/icons/Pen.icon";
-import { Editor } from "src/shared/components/Editor";
 import { useChatStore } from "src/shared/providers";
+
+// Shared components
+import { Editor } from "src/shared/components/Editor";
+
+// Icons
 import CrossIcon from "src/shared/icons/Cross.icon";
+import PenIcon from "src/shared/icons/Pen.icon";
 import SendIcon from "src/shared/icons/Send.icon";
+import { ReactComponent as Logo } from "src/assets/icons/general-logo.svg";
+
+// Chat message utilities
+import { parseContent } from "src/components/chat-message/parseContent";
+import { parseTextFormatting } from "src/components/chat-message/parseTextFormatting";
+import ChartRenderer from "src/components/chat-message/parseChart";
+
+// Styles
 import css from "./ChatMessage.module.less";
 
 interface Props {
     data: IMessage
+    editor: EditorTiptap | null;
 }
 
 export const ChatMessage: React.FC<Props> = ({
@@ -17,6 +36,8 @@ export const ChatMessage: React.FC<Props> = ({
     const [isEdit, setEdit] = React.useState(false);
     const [content, setContent] = React.useState(data.content);
     const { editor, setEditor } = useChatStore();
+    const parsedContent = parseContent(content);
+    const messageRef = React.useRef<HTMLDivElement>(null);
 
     const toggleEdit = () => {
         setEdit(!isEdit);
@@ -67,9 +88,39 @@ export const ChatMessage: React.FC<Props> = ({
     }
     if(data.isCode) {
         return (
-            <div className={css.message}>
-                    {data.content}
+            <div className={`${css.chat_message} ${data.isUser ? css.user_message : css.bot_message}`}>
+                {!data.isUser && (
+                    <div className={css.bot_logo}>
+                        <Logo />
+                    </div>
+                )}
+            <div className={css.message_content}>
+                <div ref={messageRef}>
+                    <MathJax>
+                        {parsedContent.map((part, index) => {
+                            if (part.type === "text" && !data.isUser) {
+                                return (
+                                    <div
+                                        dangerouslySetInnerHTML={{
+                                            __html: parseTextFormatting(part.content),
+                                        }}
+                                    />
+                                );
+                            } else if (part.type === "chart" && !data.isUser) {
+                                return <ChartRenderer key={index} input={part.content} />;
+                            }
+                            return (
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: parseTextFormatting(part.content),
+                                    }}
+                                />
+                            );
+                        })}
+                    </MathJax>
+                </div>
             </div>
+        </div>
         )
     }
 
