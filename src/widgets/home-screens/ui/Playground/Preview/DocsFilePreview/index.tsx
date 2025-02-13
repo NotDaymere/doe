a9bdfe255195d56b98180dc755dfe167c3c0c2e6
Yulia Osadchuk <yulia.zelenskaya@gmail.com>
2025-02-13 12:34:08 +0200

    add preview for docx files, fix styles

--- src/widgets/home-screens/ui/Playground/Preview/DocsFilePreview/index.tsx
+++ src/widgets/home-screens/ui/Playground/Preview/DocsFilePreview/index.tsx
@@ -1,6 +1,7 @@
-import ArrowRightUp from "src/shared/icons/ArrowRightUp.icon";
+import ArrowRightUpIcon from "src/shared/icons/ArrowRightUp.icon";
 import { GlobalWorkerOptions } from "pdfjs-dist";
-import { useState, useEffect, Fragment } from "react";
+import mammoth from "mammoth";
+import { useState, useEffect } from "react";
 import { Viewer } from "@react-pdf-viewer/core";
 import "@react-pdf-viewer/core/lib/styles/index.css";
 import "@react-pdf-viewer/core/lib/styles/index.css";
@@ -15,7 +16,7 @@ interface IProps {
 const DocsPreview: React.FC<IProps> = ({ url }) => {
     const fileType = url?.split(".").pop() || "";
     const [textContent, setTextContent] = useState<any>(null);
-    const [isHovered, setIsHovered] = useState(true);
+    const [isHovered, setIsHovered] = useState(false);
 
     useEffect(() => {
         if (fileType === "txt") {
@@ -23,15 +24,16 @@ const DocsPreview: React.FC<IProps> = ({ url }) => {
                 .then((response) => response.text())
                 .then((data) => setTextContent(data))
                 .catch((error) => console.error(error));
+        } else if (fileType === "docx") {
+            setTextContent(null);
+            fetch(url)
+                .then((response) => response.arrayBuffer())
+                .then((arrayBuffer) => mammoth.convertToHtml({ arrayBuffer }))
+                .then((result) => {
+                    setTextContent(result.value);
+                })
+                .catch((error) => console.error(error));
         }
-        // else if (fileType === "docx") {
-        //     // Fetch and render DOCX file content
-        //     fetch(url)
-        //         .then((response) => response.arrayBuffer())
-        //         .then((arrayBuffer) => mammoth.convertToHtml({ arrayBuffer }))
-        //         .then((result) => setDocContent(result.value))
-        //         .catch((error) => console.error(error));
-        // }
     }, [url, fileType]);
 
     const handleOpenInNewWindowClick = (e: any) => {
@@ -47,7 +49,7 @@ const DocsPreview: React.FC<IProps> = ({ url }) => {
                     rel="noreferrer"
                     onClick={(e) => handleOpenInNewWindowClick(e)}
                 >
-                    <ArrowRightUp width={16} height={16} />
+                    <ArrowRightUpIcon width={16} height={16} />
                     <span>Open in new window</span>
                 </button>
             )}
@@ -71,7 +73,6 @@ const DocsPreview: React.FC<IProps> = ({ url }) => {
             case "txt":
                 return (
                     <pre
-                        id="txtPreview"
                         className={css.preview}
                         onDoubleClick={() => console.log("DocsPreview")}
                         onMouseEnter={() => setIsHovered(true)}
@@ -81,12 +82,17 @@ const DocsPreview: React.FC<IProps> = ({ url }) => {
                         {renderOpenInNewWindowButtonClick()}
                     </pre>
                 );
-            case "doc":
+            case "docx":
                 return (
-                    <>
-                        {url}
+                    <pre
+                        onDoubleClick={() => console.log("DocsPreview")}
+                        onMouseEnter={() => setIsHovered(true)}
+                        onMouseLeave={() => setIsHovered(false)}
+                        className={css.preview}
+                    >
+                        {textContent}
                         {renderOpenInNewWindowButtonClick()}
-                    </>
+                    </pre>
                 );
             default:
                 return null;
