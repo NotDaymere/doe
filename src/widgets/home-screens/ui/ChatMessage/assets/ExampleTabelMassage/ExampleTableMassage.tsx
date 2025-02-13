e92b375a61c2fccc4619886761c0f6cfdc0eeafc
Yulii Morozov <morozovyulii@gmail.com>
2025-02-13 14:05:33 +0200

    add styles to example table massage

--- src/widgets/home-screens/ui/ChatMessage/assets/ExampleTabelMassage/ExampleTableMassage.tsx
+++ src/widgets/home-screens/ui/ChatMessage/assets/ExampleTabelMassage/ExampleTableMassage.tsx
@@ -3,15 +3,16 @@ import { Flex } from "antd";
 import TableRandomValues from "../TableRandomValues/TableRandomValues";
 import DownloadCSV from "../DownloadCSV/DownloadCSV";
 import React from "react";
+import './ExampleTableMassage.less';
 
 function ExampleTableMassage() {
     return (
-        <div>
-            <text>
+        <div className={"example-table-massage-container"}>
+            <text className={"message-text"}>
                 Here's a simple project idea: a Task Manager command-line application in Python. It will allow you to add, view, and delete tasks. In the structure, we'll be able to add and view all tasks, delete tasks by number:
             </text>
             <ExampleCode />
-            <text>Now Ill show the output in the table:</text>
+            <text className={'message-text'}>Now Ill show the output in the table:</text>
             <Flex justify={"flex-start"} className={"message-actions"}>
                 <TableRandomValues />
 
--- src/widgets/home-screens/ui/ChatMessage/assets/ExampleTabelMassage/assets/ExampleCode/ExampleCode.tsx
+++ src/widgets/home-screens/ui/ChatMessage/assets/ExampleTabelMassage/assets/ExampleCode/ExampleCode.tsx
@@ -1,9 +1,16 @@
+import './ExampleCode.less';
+
 function ExampleCode() {
-    return (<div>
-        <p><p>def</p> <p>delete_element</p><p>(my_list, element):</p>
-        <p>"""Removes the first occurrence of the element from the list."""</p>
+    return (<div className={"example-code-container"}>
+        <p className={'code'}>
+            <p className={'dif-params'}>
+                <p className={'def'}>def</p>
+                <p className={'dif-name'}>delete_element</p>
+                <p className={'dif-value'}>(my_list, element):</p>
+            </p>
+        <p className={'dif-content'}>"""Removes the first occurrence of the element from the list."""</p>
         </p>
-        <div>
+        <div className={'example-code-buttons'}>
             <button>value</button>
             <button>value</button>
             <button>value</button>
