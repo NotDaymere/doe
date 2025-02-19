45caaad63bab353f2da3be395da943cc66aafce2
YuriiHolyda <mescyura@gmail.com>
2025-02-19 18:37:54 +0200

    used icons from shared, add hover appear for icons

--- src/widgets/home-screens/ui/ChatMessage/index.tsx
+++ src/widgets/home-screens/ui/ChatMessage/index.tsx
@@ -30,15 +30,16 @@ import ChartRenderer from "src/components/chat-message/parseChart";
 // Styles
 import css from "./ChatMessage.module.less";
 import "highlight.js/styles/github-dark.css";
+import PlayIcon from "src/shared/icons/Play.icon";
+import DownloadIcon from "src/shared/icons/Download.icon";
+import CopyIcon from "src/shared/icons/Copy.icon";
 
 interface Props {
-    data: IMessage
+    data: IMessage;
     editor: EditorTiptap | null;
 }
 
-export const ChatMessage: React.FC<Props> = ({
-    data
-}) => {
+export const ChatMessage: React.FC<Props> = ({ data }) => {
     const [isEdit, setEdit] = React.useState(false);
     const [content, setContent] = React.useState(data.content);
     const { editor, setEditor } = useChatStore();
@@ -53,68 +54,68 @@ export const ChatMessage: React.FC<Props> = ({
             });
         }
     }, [content, messageRef]);
-    const [isPaused,setIsPaused] = React.useState(true);
+    const [isPaused, setIsPaused] = React.useState(true);
     const [utterance, setUtterance] = React.useState<SpeechSynthesisUtterance | null>(null);
     //speech
     const synth = React.useRef(window.speechSynthesis);
     React.useEffect(() => {
         const synth = window.speechSynthesis;
-    
+
         const initUtterance = () => {
-          const textToSpeak = messageRef.current?.textContent || "";
-          const u = new SpeechSynthesisUtterance(textToSpeak);
-    
-          const voices = synth.getVoices();
-          if (voices.length > 0) {
-            u.voice = voices.find((v) => v.lang.startsWith("en")) || voices[0];
-          }
-    
-          setUtterance(u);
+            const textToSpeak = messageRef.current?.textContent || "";
+            const u = new SpeechSynthesisUtterance(textToSpeak);
+
+            const voices = synth.getVoices();
+            if (voices.length > 0) {
+                u.voice = voices.find((v) => v.lang.startsWith("en")) || voices[0];
+            }
+
+            setUtterance(u);
         };
-    
+
         initUtterance();
         //voice updating
         const handleVoicesChanged = () => {
-          initUtterance();
+            initUtterance();
         };
-    
-        synth.addEventListener("voiceschanged", handleVoicesChanged);
-    
-        return () => {
-          synth.cancel();
-          synth.removeEventListener("voiceschanged", handleVoicesChanged);
-        };
-      }, []);
-    
-    const handlePlay = () => {
-    if (!utterance) {
-      console.error("Utterance is not ready.");
-      return;
-    }
 
-    if (synth.current.speaking) {
-      console.warn("Already speaking.");
-      return;
-    }
-    
-    synth.current.speak(utterance);
-    setIsPaused(false);
+        synth.addEventListener("voiceschanged", handleVoicesChanged);
+
+        return () => {
+            synth.cancel();
+            synth.removeEventListener("voiceschanged", handleVoicesChanged);
+        };
+    }, []);
+
+    const handlePlay = () => {
+        if (!utterance) {
+            console.error("Utterance is not ready.");
+            return;
+        }
+
+        if (synth.current.speaking) {
+            console.warn("Already speaking.");
+            return;
+        }
+
+        synth.current.speak(utterance);
+        setIsPaused(false);
     };
 
-  const handleStop = () => {
-    synth.current.cancel();
+    const handleStop = () => {
+        synth.current.cancel();
 
-    setIsPaused(true);
-  };
+        setIsPaused(true);
+    };
 
     const toggleEdit = () => {
         setEdit(!isEdit);
-    }
+    };
 
     const cancelEdit = () => {
         setContent(data.content);
         setEdit(false);
-    }
+    };
     const handleCopy = () => {
         if (messageRef.current) {
             const range = document.createRange();
@@ -126,36 +127,35 @@ export const ChatMessage: React.FC<Props> = ({
             selection?.removeAllRanges();
         }
     };
-        const downloadPDF = () => {
-            if (messageRef.current) {
-                const doc = new jsPDF();
-    
-                const content = messageRef.current;
-    
-                doc.html(content, {
-                    callback: function (doc) {
-                        doc.save("response.pdf");
-                    },
-                    html2canvas: { scale: 0.3 },
-                    x: 10,
-                    y: 10,
-                });
-            }
-        };
-        const openSourcePlayground = () => {
-            setPlayground((prev) => ({
-                ...prev,
-                type: "source",
-                open: true,
-            }));
-        };
+    const downloadPDF = () => {
+        if (messageRef.current) {
+            const doc = new jsPDF();
 
+            const content = messageRef.current;
 
-    if(data.isUser) {
-        if(isEdit) {
+            doc.html(content, {
+                callback: function (doc) {
+                    doc.save("response.pdf");
+                },
+                html2canvas: { scale: 0.3 },
+                x: 10,
+                y: 10,
+            });
+        }
+    };
+    const openSourcePlayground = () => {
+        setPlayground((prev) => ({
+            ...prev,
+            type: "source",
+            open: true,
+        }));
+    };
+
+    if (data.isUser) {
+        if (isEdit) {
             return (
                 <div className={css.edit}>
-                    <Editor 
+                    <Editor
                         value={content}
                         onChange={setContent}
                         onFocus={setEditor}
@@ -173,25 +173,31 @@ export const ChatMessage: React.FC<Props> = ({
                         </button>
                     </div>
                 </div>
-            )
+            );
         }
 
         return (
             <div className={css.input}>
                 <button className={css.input_editBtn} onClick={toggleEdit}>
-                    <PenIcon />
+                    <span className={css.svg_wrapper}>
+                        <PenIcon />
+                        <span className={css.tooltip}>Edit</span>
+                    </span>
                 </button>
-                <div className={css.input_message} 
+                <div
+                    className={css.input_message}
                     dangerouslySetInnerHTML={{
-                        __html: data.content
+                        __html: data.content,
                     }}
                 />
             </div>
-        )
+        );
     }
-    if(data.isCode) {
+    if (data.isCode) {
         return (
-            <div className={`${css.chat_message} ${data.isUser ? css.user_message : css.bot_message}`}>
+            <div
+                className={`${css.chat_message} ${data.isUser ? css.user_message : css.bot_message}`}
+            >
                 {!data.isUser && (
                     <div className={css.bot_logo_background}>
                         <div className={css.bot_logo}>
@@ -199,11 +205,21 @@ export const ChatMessage: React.FC<Props> = ({
                         </div>
                     </div>
                 )}
-            <div className={css.message_content}>
-                <div ref={messageRef}>
-                    <MathJax>
-                        {parsedContent.map((part, index) => {
-                            if (part.type === "text" && !data.isUser) {
+                <div className={css.message_content}>
+                    <div ref={messageRef}>
+                        <MathJax>
+                            {parsedContent.map((part, index) => {
+                                if (part.type === "text" && !data.isUser) {
+                                    return (
+                                        <div
+                                            dangerouslySetInnerHTML={{
+                                                __html: parseTextFormatting(part.content),
+                                            }}
+                                        />
+                                    );
+                                } else if (part.type === "chart" && !data.isUser) {
+                                    return <ChartRenderer key={index} input={part.content} />;
+                                }
                                 return (
                                     <div
                                         dangerouslySetInnerHTML={{
@@ -211,49 +227,44 @@ export const ChatMessage: React.FC<Props> = ({
                                         }}
                                     />
                                 );
-                            } else if (part.type === "chart" && !data.isUser) {
-                                return <ChartRenderer key={index} input={part.content} />;
-                            }
-                            return (
-                                <div
-                                    dangerouslySetInnerHTML={{
-                                        __html: parseTextFormatting(part.content),
-                                    }}
+                            })}
+                        </MathJax>
+                    </div>
+                    {!data.isUser && (
+                        <Flex justify={"space-between"} className={"message-actions"}>
+                            <button onClick={openSourcePlayground} className={css.button_steps}>
+                                <SvgIcon
+                                    style={{ width: "15px", height: "15px", marginRight: "2px" }}
+                                    type={"seeAllStepsVioletIcon"}
                                 />
-                            );
-                        })}
-                    </MathJax>
-                </div>
-                {!data.isUser && (
-                            <Flex justify={"space-between"} className={"message-actions"}>
-                                <button onClick={openSourcePlayground} className={css.button_steps}>
-                                    <SvgIcon
-                                        style={{ width: "15px", height: "15px", marginRight: "2px" }}
-                                        type={"seeAllStepsVioletIcon"}
-                                    /> 
-                                    <span className={css.button_steps_label}>See all steps</span>
+                                <span className={css.button_steps_label}>See all steps</span>
+                            </button>
+                            <Flex gap={10}>
+                                <button
+                                    className={`${css.button_steps_grey} ${isPaused ? css.glowing_border : ""}`}
+                                    onClick={isPaused ? handlePlay : handleStop}
+                                >
+                                    <span className={css.tooltip}>Listen answer</span>
+
+                                    <PlayIcon />
                                 </button>
-                                <Flex gap={10}>
-                                    <button onClick={isPaused ? handlePlay : handleStop}>
-                                        <SvgIcon style={{ width: "100%", height: "100%" }} type={"listenIcon"} />
-                                    </button>
-                                    <button onClick={downloadPDF}>
-                                        <SvgIcon style={{ width: "100%", height: "100%" }} type={"downloadGreen"} />
-                                    </button>
-                                    <button onClick={handleCopy}>
-                                        <SvgIcon style={{ width: "100%", height: "100%" }} type={"copyAnswerGreenIcon"} />
-                                    </button>
-                                </Flex>
+                                <button onClick={downloadPDF} className={css.button_steps_green}>
+                                    <span className={css.tooltip}>Download chat text</span>
+
+                                    <DownloadIcon />
+                                </button>
+                                <button onClick={handleCopy} className={css.button_steps_green}>
+                                    <span className={css.tooltip}>Copy chat text</span>
+
+                                    <CopyIcon />
+                                </button>
                             </Flex>
-                        )}
+                        </Flex>
+                    )}
+                </div>
             </div>
-        </div>
-        )
+        );
     }
 
-    return (
-        <div className={css.message}>
-            {null}
-        </div>
-    );
+    return <div className={css.message}>{null}</div>;
 };
