548048c4a213c83d92a7bd30a7ca876a994115bf
Yulii Morozov <morozovyulii@gmail.com>
2025-02-20 23:17:52 +0200

    add fullscreen chat move

--- src/widgets/home-screens/ui/TablePlayground/assets/FullscreenGeneralLogo/FullscreenChat/FullscreenChat.tsx
+++ src/widgets/home-screens/ui/TablePlayground/assets/FullscreenGeneralLogo/FullscreenChat/FullscreenChat.tsx
@@ -1,21 +1,63 @@
+import { useState, useRef, useEffect } from "react";
 import { Flex } from "antd";
 import { ChatLayout } from "../../../../ChatLayout";
-import { ReactComponent as DecreasePlaygroundIcon } from "src/assets/icons/decrease-playground-active.svg"
-import './FullscreenChat.less';
+import { ReactComponent as DecreasePlaygroundIcon } from "src/assets/icons/decrease-playground-active.svg";
+import "./FullscreenChat.less";
 
 interface Props {
-    onClick?: () => void
+    onClick?: () => void;
 }
-export default function FullscreenChat({onClick}: Props) {
+
+export default function FullscreenChat({ onClick }: Props) {
+    const chatRef = useRef<HTMLDivElement>(null);
+    const [position, setPosition] = useState({ x: 15, y: 50 });
+    const [isDragging, setIsDragging] = useState(false);
+
+    const handleMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (event.button !== 2) return;
+        setIsDragging(true);
+    };
+
+    useEffect(() => {
+        const handleMouseMove = (event: MouseEvent) => {
+            if (!isDragging) return;
+            setPosition((prev) => ({
+                x: prev.x + event.movementX,
+                y: prev.y + event.movementY,
+            }));
+        };
+
+        const handleMouseUp = () => {
+            setIsDragging(false);
+        };
+
+        if (isDragging) {
+            window.addEventListener("mousemove", handleMouseMove);
+            window.addEventListener("mouseup", handleMouseUp);
+        }
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+            window.removeEventListener("mouseup", handleMouseUp);
+        };
+    }, [isDragging]);
+
     return (
-        <Flex vertical>
-            <button
-                onClick={onClick}
-                className={"decrease-chat-button"}
-            >
-            <DecreasePlaygroundIcon />
-            </button>
-            <ChatLayout />
-        </Flex>
-    )
+        <div
+
+        >
+            <Flex vertical>
+                <button onClick={onClick} className={"decrease-chat-button"}>
+                    <DecreasePlaygroundIcon />
+                </button>
+                <div
+                    ref={chatRef}
+                    style={{ position: "absolute", top: position.y || '-1px', left: position.x || '1px', cursor: "grab" }}
+                    onMouseDown={handleMouseDown}
+                >
+                    <ChatLayout />
+                </div>
+            </Flex>
+        </div>
+    );
 }
