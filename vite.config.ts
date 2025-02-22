6edb676e09d38b314c78df3c895c7ea374d9ebf8
Yulia Osadchuk <yulia.zelenskaya@gmail.com>
2025-02-22 17:03:15 +0200

    configuration for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,58 +1,66 @@
-import { configureReact } from "@bn-digital/vite";
+// import { configureReact } from "@bn-digital/vite";
 
-export default configureReact(
-    { 
-        server: { 
-            hmr: { 
-                overlay: true 
-            } 
-        }, 
-        css: {
-            preprocessorOptions: {
-                less: {
-                    additionalData: `
-                        @import "@/styles/mixins.less";
-                        @import "@/styles/build-variables.less";
-                    `,
-                }
-            }
-        },
-        resolve: {
-            alias: {
-                '@': "/src"
-            }
-        }
-    },
-    {
-        openGraph: {
-            enabled: false,
-        },
-        pwa: {
-            enabled: false,
-        },
-        buildInfo: {
-            enabled: false,
-        },
-        react: {
-            svg: {
-                enabled: true,
-            },
-        },
-        // lint: {
-        //     enabled: true,
-        //     stylelint: false,
-        //     enableBuild: true,
-        // },
-        graphql: {
-            enabled: true,
-        },
-        // analytics: { enableDev: true },
-        // fonts: {
-        //     google: {
-        //         preconnect: true,
-        //         families: [{ name: "Lato", styles: "wght@400;500;600;700;800;900", defer: true }],
-        //         display: "auto",
-        //     },
-        // },
-    }
-);
+// export default configureReact(
+//     {
+//         server: {
+//             hmr: {
+//                 overlay: true
+//             }
+//         },
+//         css: {
+//             preprocessorOptions: {
+//                 less: {
+//                     additionalData: `
+//                         @import "@/styles/mixins.less";
+//                         @import "@/styles/build-variables.less";
+//                     `,
+//                 }
+//             }
+//         },
+//         resolve: {
+//             alias: {
+//                 '@': "/src"
+//             }
+//         }
+//     },
+//     {
+//         openGraph: {
+//             enabled: false,
+//         },
+//         pwa: {
+//             enabled: false,
+//         },
+//         buildInfo: {
+//             enabled: false,
+//         },
+//         react: {
+//             svg: {
+//                 enabled: true,
+//             },
+//         },
+//         // lint: {
+//         //     enabled: true,
+//         //     stylelint: false,
+//         //     enableBuild: true,
+//         // },
+//         graphql: {
+//             enabled: true,
+//         },
+//         // analytics: { enableDev: true },
+//         // fonts: {
+//         //     google: {
+//         //         preconnect: true,
+//         //         families: [{ name: "Lato", styles: "wght@400;500;600;700;800;900", defer: true }],
+//         //         display: "auto",
+//         //     },
+//         // },
+//     }
+// );
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
+});
