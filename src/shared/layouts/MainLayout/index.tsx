f1316580b9a72c464308cb4cd140b94bf692a657
maksymtrinkal <maksymtrinkal@gobuild360.io>
2025-03-03 19:50:50 +0200

    minor changes

--- src/shared/layouts/MainLayout/index.tsx
+++ src/shared/layouts/MainLayout/index.tsx
@@ -19,15 +19,14 @@ export const MainLayout: React.FC<Props> = ({
     const {openHistory} = useVersionHistoryStore();
     return (
         <React.Fragment>
+            <Gaia className={'gaia'} ref={gaiaRef} />
             <BaseLayout className={getOpenSavedPlaygrounds().length > 0 ? "main-layout-playground" : "main-layout" } hasSider>
                 <BaseLayout.Sider width={"auto"} className={"sider-wrapper"}>
                     <Sider />
                 </BaseLayout.Sider>
-                {/*//TODO оберни чилдрен в контейнер и присваивай контейнера макс видс 0 оверфлоу хиден с транзишином 0ю5 а по умлчанию натрой фит контент или что то типо*/}
                 <div className={`children-main-layout ${((!playgroundFullscreen) || getOpenSavedPlaygrounds().length == 0) && 'children-main-layout-active'}`}>
                     {children}
                 </div>
-            <Gaia className={'gaia'} ref={gaiaRef} />
             {getOpenSavedPlaygrounds().length > 0 && (
                 <>
                     <BaseLayout.Sider width={"inherit"}  className={`playground-sider ${playgroundFullscreen ? (openHistory? 'width-80' : 'width-100') : "width-550px"}`}>
