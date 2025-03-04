ad091d133fd8014ceff5430e22b3776acd56c1ac
maksymtrinkal <maksymtrinkal@gobuild360.io>
2025-03-04 23:53:31 +0200

    add file type definition and distribution to the appropriate modals
    # Conflicts:
    #	src/shared/components/FileList/index.tsx

--- src/shared/components/FileList/index.tsx
+++ src/shared/components/FileList/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import css from "./FileList.module.less";
 import clsx from "clsx";
 import { FileItem } from "../FileItem";
@@ -10,59 +10,31 @@ interface Props {
 }
 
 export const FileList: React.FC<Props> = ({
-    files,
-    onChange,
-    className
-}) => {
+                                              files,
+                                              onChange,
+                                              className
+                                          }) => {
+    const [fileURLs, setFileURLs] = useState<Record<string, string>>({});
 
-    const containerRef = React.useRef<HTMLDivElement>(null);
-    const [isDragging, setIsDragging] = React.useState(false);
-    const startX = React.useRef(0);
-    const scrollLeft = React.useRef(0);
+    useEffect(() => {
+        const urls: Record<string, string> = {};
+        files.forEach(file => {
+            urls[file.name] = URL.createObjectURL(file);
+        });
+        setFileURLs(urls);
 
-    const onMouseDown = (e: React.MouseEvent) => {
-        if (!containerRef.current) return;
-        setIsDragging(true);
-
-        startX.current = e.pageX - containerRef.current.offsetLeft;
-        scrollLeft.current = containerRef.current.scrollLeft;
-        containerRef.current.style.cursor = "grabbing";
-    };
-
-    const onMouseMove = (e: React.MouseEvent) => {
-        if (!isDragging || !containerRef.current) return;
-        e.preventDefault();
-        const x = e.pageX - containerRef.current.offsetLeft;
-        const walk = x - startX.current;
-        containerRef.current.scrollLeft = scrollLeft.current - walk;
-    };
-
-    const onMouseUpOrLeave = () => {
-        if (!containerRef.current) return;
-        setIsDragging(false);
-        containerRef.current.style.cursor = "grab";
-    };
-
-
-    React.useEffect(() => {
-        if (containerRef.current) {
-            containerRef.current.style.cursor = "grab";
-        }
-    }, []);
+        return () => {
+            Object.values(urls).forEach(url => URL.revokeObjectURL(url));
+        };
+    }, [files]);
 
     return (
-        <div
-            className={clsx(css.files, "scrollbar", className)}
-             ref={containerRef}
-             onMouseDown={onMouseDown}
-             onMouseMove={onMouseMove}
-             onMouseUp={onMouseUpOrLeave}
-             onMouseLeave={onMouseUpOrLeave}
-        >
+        <div className={clsx(css.files, "scrollbar", className)}>
             {files.map((file, id) => (
-                <FileItem 
+                <FileItem
                     name={file.name}
                     mimetype={file.type}
+                    url={fileURLs[file.name]}
                     onDelete={() => onChange(files.filter((item) => item !== file))}
                     key={file.name + id}
                 />
