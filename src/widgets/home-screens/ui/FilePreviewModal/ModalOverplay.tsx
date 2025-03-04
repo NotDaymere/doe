8c220b46093913bc7a084761fdfec94ba56f3cd9
maksymtrinkal <maksymtrinkal@gobuild360.io>
2025-03-04 23:53:31 +0200

    add file type definition and distribution to the appropriate modals
    # Conflicts:
    #	src/shared/components/FileList/index.tsx

---
+++ src/widgets/home-screens/ui/FilePreviewModal/ModalOverplay.tsx
@@ -0,0 +1,23 @@
+import React, { ReactNode } from "react";
+import css from "./ModalOverplay.module.less";
+import CrossIcon from "src/shared/icons/Cross.icon";
+
+interface ModalOverlayProps {
+    onClose: () => void;
+    children: ReactNode;
+}
+
+const ModalOverlay: React.FC<ModalOverlayProps> = ({ onClose, children }) => {
+    return (
+        <div className={css.modalOverlay} onClick={onClose}>
+            <div className={css.modalContent} onClick={(e) => e.stopPropagation()}>
+                <button className={css.modalCloseBtn} onClick={onClose}>
+                    <CrossIcon />
+                </button>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default ModalOverlay;
