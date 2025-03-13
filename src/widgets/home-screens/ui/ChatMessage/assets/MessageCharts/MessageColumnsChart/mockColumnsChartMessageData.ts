db8a1d973502a80de11b7f5833c75037e661742c
maksymtrinkal <maksymtrinkal@gobuild360.io>
2025-03-13 22:10:04 +0200

    minor changes

--- src/widgets/home-screens/ui/ChatMessage/assets/MessageCharts/MessageColumnsChart/mockColumnsChartMessageData.ts
+++ src/widgets/home-screens/ui/ChatMessage/assets/MessageCharts/MessageColumnsChart/mockColumnsChartMessageData.ts
@@ -8,7 +8,7 @@ export interface ChartMessageData {
 
 export const mockColumnsChartMessageData: ChartMessageData[] = [
     { id: 'bcm', label: 'BCM (projected)', value: 2206, percentile: 99 },
-    { id: 'preview', label: 'o1 preview', value: 1258, percentile: 69 },
+    { id: 'preview', label: 'o1 preview', value: 1258, percentile: 62 },
     { id: 'o1', label: 'o1', value: 1673, percentile: 89 },
     { id: 'o1ioi', label: 'o1+ioi', value: 1470, percentile: 73 },
 ];
--- src/widgets/home-screens/ui/ChatMessage/index.tsx
+++ src/widgets/home-screens/ui/ChatMessage/index.tsx
@@ -460,7 +460,9 @@ export const ChatMessage: React.FC<Props> = ({ data, editMsgMode, setEditMsgMode
                                 );
                             })}
                         </MathJax>
-                        <MessageLineChart data={mockLineChartMessageData}/>
+                        <text>Now I’ll plot the output inline instead of using code:</text>
+                        <MessageLineChart data={mockLineChartMessageData} />
+                        <text>Now I’ll plot the output inline instead of using code:</text>
                         <MessageChart data={mockChartMessageData}/>
                         <MessageColumnsChart data={mockColumnsChartMessageData} />
                         <text className={"message-text"}>
