d9db2131b01d86941aba49d096ad5ee32f8bce94
Dallas1510 <romakreg@gmail.com>
2025-03-19 11:22:14 +0200

    finish dark mode

--- src/widgets/home-screens/ui/ChatMessage/assets/ExampleTabelMassage/ExampleTableMassage.tsx
+++ src/widgets/home-screens/ui/ChatMessage/assets/ExampleTabelMassage/ExampleTableMassage.tsx
@@ -3,7 +3,7 @@ import { Flex } from "antd";
 import TableRandomValues from "../TableRandomValues/TableRandomValues";
 import DownloadCSV from "../DownloadCSV/DownloadCSV";
 import React from "react";
-import './ExampleTableMassage.less';
+import "./ExampleTableMassage.less";
 import PythonTaskManager from "../PythonTaskManager/PythonTaskManager";
 import GeneralLogo from "../../../GeneralLogo/GeneralLogo";
 import { useChatStore } from "src/shared/providers";
@@ -15,21 +15,23 @@ function ExampleTableMassage() {
             {!playgroundFullscreen && <GeneralLogo />}
             <div className={"example-table-massage"}>
                 <text className={"message-text"}>
-                    Here's a simple project idea: a Task Manager command-line application in Python. It will allow you to add, view, and delete tasks. In the structure, we'll be able to add and view all tasks, delete tasks by number:
+                    Here's a simple project idea: a Task Manager command-line application in Python.
+                    It will allow you to add, view, and delete tasks. In the structure, we'll be
+                    able to add and view all tasks, delete tasks by number:
                 </text>
                 <ExampleCode />
-                <text className={'message-text'}>Now Ill show the output in the table:</text>
+                <text className={"message-text"}>Now Ill show the output in the table:</text>
                 <Flex justify={"flex-start"} className={"message-actions"} vertical>
                     <Flex>
                         <TableRandomValues />
                         <DownloadCSV />
                     </Flex>
                     <Flex>
-                    <PythonTaskManager/>
+                        <PythonTaskManager />
                     </Flex>
                 </Flex>
             </div>
         </div>
-    )
+    );
 }
-export default ExampleTableMassage
+export default ExampleTableMassage;
