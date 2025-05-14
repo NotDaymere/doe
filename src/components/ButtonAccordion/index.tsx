5147cb136feefa26e7e3b3f27d2457ffa6c03452
oleksii-prokopchenko-cubex <oleksprokopchenko@gmail.com>
2025-05-14 20:41:16 +0300

    ux: decrease interval between screen sharing buttons highlighting

--- src/components/ButtonAccordion/index.tsx
+++ src/components/ButtonAccordion/index.tsx
@@ -21,7 +21,7 @@ export const ButtonAccordion = ({
                 clearInterval(interval);
                 handleCloseScreenSharing();
             }
-        }, 2000);
+        }, 1000);
 
         return () => clearInterval(interval);
     }, []);
