185d9bb7811c514ca5b4184bb6cdb824da0e0780
yulii-morozov <morozovyulii@gmail.com>
2025-06-27 13:53:24 +0300

    fix:input

--- src/widgets/home-screens/ui/TablePlayground/assets/OpenFromSavedPlayground/OpenFromSavedPlayground.tsx
+++ src/widgets/home-screens/ui/TablePlayground/assets/OpenFromSavedPlayground/OpenFromSavedPlayground.tsx
@@ -3,9 +3,6 @@ import { ReactComponent as TableIcon } from "src/assets/icons/table.svg";
 import { ReactComponent as CodeIcon } from "src/assets/icons/code.svg";
 import './OpenFromSavedPlayground.less';
 import { useChatStore } from "src/shared/providers";
-import MagicIcon from "../../../../../../shared/icons/Magic.icon";
-import LinkIcon from "../../../../../../shared/icons/Link.icon";
-import FileIcon from "../../../../../../shared/icons/File.icon";
 
 interface Props {
     savedPlayground: IPlayground;
